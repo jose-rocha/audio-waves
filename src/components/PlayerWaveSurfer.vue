8f97c37232f9088354c0a418e90350309195c429
José Rocha <josemarlerocha@hotmail.com>
2024-12-11 20:56:40 -0300

    Adicionado o componente usando a lib WaveSurf

---
+++ src/components/PlayerWaveSurfer.vue
@@ -0,0 +1,143 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import WaveSurfer from 'wavesurfer.js';
+// Usei esse de exemplo https://wavesurfer.xyz/examples/?zoom.js
+// e esse https://wavesurfer.xyz/examples/?all-options.js
+import { getCssVar } from 'quasar';
+
+const props = defineProps<{ player: HTMLAudioElement }>();
+const isPlaying = ref(false);
+const showWavesFake = ref(true);
+const audioDuration = ref(0);
+const audioInProgress = ref();
+const wavesurfer = ref();
+const audioSpeed = ref([1, 1.5, 2]);
+const audioSpeedValue = ref(0);
+
+const playAudio = () => {
+  isPlaying.value = true;
+  wavesurfer.value.play();
+
+  setTimeout(() => {
+    showWavesFake.value = false;
+  }, 1000);
+};
+
+const pauseAudio = () => {
+  wavesurfer.value.pause();
+  isPlaying.value = false;
+};
+
+const calculateTime = (secs: number) => {
+  const minutes = Math.floor(secs / 60);
+  const seconds = Math.floor(secs % 60);
+  const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+  return `${minutes}:${returnedSeconds}`;
+};
+
+const setPlaybackRate = () => {
+  if (audioSpeedValue.value < 2) {
+    audioSpeedValue.value += 1;
+    wavesurfer.value?.setPlaybackRate(audioSpeed.value[audioSpeedValue.value], true);
+  } else {
+    audioSpeedValue.value = 0;
+    wavesurfer.value?.setPlaybackRate(audioSpeed.value[audioSpeedValue.value], true);
+  }
+};
+
+onMounted(() => {
+  wavesurfer.value = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: '#BDBDBD',
+    progressColor: `${getCssVar('primary')}`,
+    url: props.player.src,
+    barWidth: 5, // largura da barra
+    barHeight: 1, // altura da barra
+    height: 40, // altura do waveform
+    cursorWidth: 2, // largura do cursor
+    barGap: 1,
+    barRadius: 2,
+    audioRate: 1,
+  });
+
+  wavesurfer.value.on('interaction', () => {
+    console.log('clicou nas ondas');
+    wavesurfer.value.play();
+    isPlaying.value = true;
+  });
+
+  // https://wavesurfer.xyz/examples/?soundcloud.js
+  wavesurfer.value.on('decode', (duration: number) => {
+    // console.log('decode', duration);
+    audioDuration.value = duration;
+  });
+  wavesurfer.value?.on('timeupdate', (currentTime: number) => {
+    audioInProgress.value = currentTime;
+  });
+
+  wavesurfer.value?.on('finish', () => {
+    isPlaying.value = false;
+  });
+});
+</script>
+
+<template>
+  <div class="full-width flex flex-center" style="justify-content: center">
+    <div
+      ref="audio-player-container"
+      class="row"
+      style="background-color: #2a3942; width: 70%; height: 50px; border-radius: 5px"
+    >
+      <div class="col full-height flex flex-center">
+        <q-icon
+          v-if="!isPlaying"
+          name="mdi-play"
+          color="primary"
+          size="40px"
+          class="cursor-pointer"
+          @click="playAudio"
+        />
+
+        <q-icon
+          v-else
+          name="mdi-pause"
+          color="primary"
+          size="40px"
+          class="cursor-pointer"
+          @click="pauseAudio"
+        />
+      </div>
+
+      <div class="col-8 full-height flex justify-center items-center">
+        <div
+          class="col-2 full-height flex flex-center"
+          style="color: #bdbdbd; transform: translateY(2px)"
+        >
+          {{ !audioInProgress ? calculateTime(audioDuration) : calculateTime(audioInProgress) }}
+        </div>
+        <!--  Waves -->
+        <div id="waveform" class="col q-px-sm cursor-pointer"></div>
+      </div>
+
+      <div class="col full-height flex flex-center">
+        <q-avatar v-if="isPlaying" @click="setPlaybackRate">
+          <!-- <slot name="icon-user" />-->
+          <q-chip color="primary" text-color="white" class="q-pa-sm col cursor-pointer" dense>
+            <q-tooltip anchor="top middle" self="bottom middle" :offset="[0, 10]">
+              Altere velocidade do áudio
+            </q-tooltip>
+            <span>
+              {{
+                audioSpeed[audioSpeedValue] !== 1.5
+                  ? `${audioSpeed[audioSpeedValue]}.0`
+                  : audioSpeed[audioSpeedValue]
+              }}x
+            </span>
+          </q-chip>
+        </q-avatar>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,43 +1,37 @@
-<template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
-  </q-page>
-</template>
-
 <script setup lang="ts">
 import { ref } from 'vue';
-import type { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
+import PlayerWaveSurfer from 'components/PlayerWaveSurfer.vue';
+// import AudioPlayerVueAudioVisual from 'components/PlayerVueAudioVisual.vue';
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1',
-  },
-  {
-    id: 2,
-    content: 'ct2',
-  },
-  {
-    id: 3,
-    content: 'ct3',
-  },
-  {
-    id: 4,
-    content: 'ct4',
-  },
-  {
-    id: 5,
-    content: 'ct5',
-  },
-]);
+const player = ref<HTMLAudioElement>();
+const playbackRate = ref(1.0);
+// const waveform = ref<HTMLDivElement>();
 
-const meta = ref<Meta>({
-  totalCount: 1200,
-});
+const setPlaybackRate = () => {
+  // playbackRate mostra a velocidade atual do áudio
+  if (player.value && player.value.playbackRate < 2.0) {
+    player.value.playbackRate += 0.5;
+    playbackRate.value += 0.5;
+
+    return;
+  }
+
+  if (player.value) player.value.playbackRate = 1;
+  playbackRate.value = 1;
+};
+
+if (player.value) player.value.addEventListener('ratechange', setPlaybackRate);
 </script>
+
+<template>
+  <q-page class="row items-center justify-evenly bg-grey">
+    <div class="full-width">
+      <h4 class="text-center">Player WaveSurfer.JS</h4>
+      <div class="flex flex-center">
+        <audio v-show="false" ref="player" src="/audio-example.mp3" controls preload="metadata" />
+      </div>
+
+      <player-wave-surfer v-if="player" :player="player" />
+    </div>
+  </q-page>
+</template>
